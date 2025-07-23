d7fe572bac34eb7d5a41602d6be879e589ae7968
Nodirbek Xonimqulov <ccnodirbekcc@gmail.com>
2025-07-23 11:57:46 +0500

    prepare for deploy

--- Todo/Todo/settings.py
+++ Todo/Todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rnj1$cdswgfmgw-cb&gh(1ms!1psoa@dh&)c1h^6d_img9$s7c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
